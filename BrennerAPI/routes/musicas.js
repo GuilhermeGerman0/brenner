7d22f0d6bc9881eb296e8ebd27d7747cb092ee6d
GuilhermeGerman0 <cc24129@g.unicamp.br>
2025-09-23 11:44:40 -0300

    vou vhorar

--- BrennerAPI/routes/musicas.js
+++ BrennerAPI/routes/musicas.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { execQuery } from '../index.js';
+
 const router = express.Router();
 
 // últimas 5 músicas
@@ -29,23 +30,25 @@ router.post('/', async (req, res) => {
   }
 });
 
-export default router;
 // atualizar música
 router.put('/:id', async (req, res) => {
   const { id } = req.params;      
-    const { nomeMusica, idArtista, album, anoLancamento } = req.body;      
-    try {
-        const result = await execQuery(
-            `UPDATE brenner.Musicas 
-             SET nomeMusica=$1, idArtista=$2, album=$3, anoLancamento=$4 
-             WHERE idMusica=$5`,    
-            [nomeMusica, idArtista, album, anoLancamento, id]
-        );  
-        if (result.rowCount === 0) {      
-            return res.status(404).json({ error: 'Música não encontrada' });      
-        }   
-        res.sendStatus(204);
-    } catch (err) {      
-        res.status(500).json({ error: 'Erro ao atualizar música' });      
+  const { nomeMusica, idArtista, album, anoLancamento } = req.body;      
+  try {
+    const result = await execQuery(
+      `UPDATE brenner.Musicas 
+       SET nomeMusica=$1, idArtista=$2, album=$3, anoLancamento=$4 
+       WHERE idMusica=$5`,    
+      [nomeMusica, idArtista, album, anoLancamento, id]
+    );  
+    if (result.rowCount === 0) {      
+      return res.status(404).json({ error: 'Música não encontrada' });      
     }   
+    res.sendStatus(204);
+  } catch (err) {      
+    res.status(500).json({ error: 'Erro ao atualizar música' });      
+  }   
 });
+
+// exporta depois de todas as rotas
+export default router;
