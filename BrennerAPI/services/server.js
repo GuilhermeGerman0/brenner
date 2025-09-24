219be2c0524309371cadded05a46cb37265b42e6
GuilhermeGerman0 <cc24129@g.unicamp.br>
2025-09-24 10:25:46 -0300

    Api

--- BrennerAPI/services/server.js
+++ BrennerAPI/services/server.js
@@ -1,25 +1,32 @@
+import 'dotenv/config'; // ESSENCIAL: Deve ser a PRIMEIRA linha
 import express from 'express';
 import cors from 'cors';
+
+// Importa os roteadores
 import usuariosRouter from '../routes/usuarios.js';
 import artistasRouter from '../routes/artistas.js';
 import musicasRouter from '../routes/musicas.js';
 import tablaturasRouter from '../routes/tablaturas.js';
-import 'dotenv/config';
 
 const app = express();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Rota de teste
+app.get('/', (req, res) => {
+  res.send('Brenner API está funcionando!');
+});
+
+// Define as rotas da API
 app.use('/usuarios', usuariosRouter);
 app.use('/artistas', artistasRouter);
 app.use('/musicas', musicasRouter);
 app.use('/tablaturas', tablaturasRouter);
 
+// Inicia o servidor
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('Brenner API está funcionando!');
-  });
-  
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
